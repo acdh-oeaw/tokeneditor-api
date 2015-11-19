eea675606680c75e155d92e21b6b253005ab64c1
tokeneditor <tokeneditor@eos>
2015-11-19 16:48:59 +0100

    Experiments

--- php/TokenArray.php
+++ php/TokenArray.php
@@ -1,32 +1,25 @@
 <?php 
 
-
-
 class TokenArray {
-function generateJSON($documentid,$con,$userid) {
-//tokens with concrete document_id to json => to json als eigene methode? //
-$query = $con->prepare("
-	SELECT json_agg(json_build_object('document_id', document_id, 'token_id', token_id, 'value', value, 'properties', properties))
-	FROM ( 
-		SELECT document_id, token_id, t.value, json_agg(json_build_object(name, COALESCE(uv.value, v.value))) AS properties
-		FROM 
-			properties 
-			JOIN orig_values v USING (document_id, property_xpath) 
-			JOIN tokens t USING (document_id, token_id) 
-			LEFT JOIN values uv USING (document_id, property_xpath, token_id
-		)
-		ORDER BY token_id
-	WHERE document_id = $documentid AND (user_id = 'bkrautgartner@oeaw.ac.at' OR user_id is NULL) 
-	GROUP BY 1, 2, 3 ) t1");
-$query->execute();
-//$result = $query->fetch(PDO::FETCH_COLUMN);
-$result = $query->fetch(PDO::FETCH_COLUMN);
-//print_r($result);
-return $result;
-
-}
-
-
-
-
+	function generateJSON($documentid,$con,$userid) {
+		//tokens with concrete document_id to json => to json als eigene methode? //
+		$query = $con->prepare("
+			SELECT json_agg(json_build_object('document_id', document_id, 'token_id', token_id, 'value', value, 'properties', properties))
+			FROM ( 
+				SELECT document_id, token_id, t.value, json_agg(json_build_object(name, COALESCE(uv.value, v.value))) AS properties
+				FROM 
+					properties 
+					JOIN orig_values v USING (document_id, property_xpath) 
+					JOIN tokens t USING (document_id, token_id) 
+					LEFT JOIN values uv USING (document_id, property_xpath, token_id
+				)
+				ORDER BY token_id
+			WHERE document_id = $documentid AND (user_id = $userid OR user_id is NULL) 
+			GROUP BY 1, 2, 3 ) t1");
+		$query->execute(array($documentid, $userid));
+		//$result = $query->fetch(PDO::FETCH_COLUMN);
+		$result = $query->fetch(PDO::FETCH_COLUMN);
+		//print_r($result);
+		return $result;
+	}
 }
--- php/generatejson.php
+++ php/generatejson.php
@@ -4,10 +4,10 @@ header('Access-Control-Allow-Headers: X-Requested-With, Content-Type');
 require_once('config.inc.php');
 include 'TokenArray.php';
 $con = new PDO($CONFIG['dbConn'], $CONFIG['dbUser'], $CONFIG['dbPasswd']);
-
+$userid = $CONFIG['userid'];
 $documentid = $_GET['docid'];
 $ta = new TokenArray();
-$json = $ta->generateJSON($documentid, $con);
+$json = $ta->generateJSON($documentid, $con, $userid);
 echo $json;
 
 
