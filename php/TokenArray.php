faa3db4870d725b9c7eec428c0fab21dfb010d9a
tokeneditor <tokeneditor@eos>
2015-11-19 16:32:51 +0100

    Preserve tokens order

--- php/TokenArray.php
+++ php/TokenArray.php
@@ -3,10 +3,21 @@
 
 
 class TokenArray {
-function generateJSON($documentid,$con) {
+function generateJSON($documentid,$con,$userid) {
 //tokens with concrete document_id to json => to json als eigene methode? //
-$query = $con->prepare("SELECT json_agg(json_build_object('document_id', document_id, 'token_id', token_id, 'value', t1.value, 'properties', properties)) FROM (SELECT document_id, token_id, t.value, json_agg(json_build_object(name, v.value)) AS properties FROM properties JOIN orig_values v USING (document_id, 
-property_xpath) JOIN tokens t USING (document_id, token_id) WHERE document_id = $documentid group by 1, 2, 3) t1");
+$query = $con->prepare("
+	SELECT json_agg(json_build_object('document_id', document_id, 'token_id', token_id, 'value', value, 'properties', properties))
+	FROM ( 
+		SELECT document_id, token_id, t.value, json_agg(json_build_object(name, COALESCE(uv.value, v.value))) AS properties
+		FROM 
+			properties 
+			JOIN orig_values v USING (document_id, property_xpath) 
+			JOIN tokens t USING (document_id, token_id) 
+			LEFT JOIN values uv USING (document_id, property_xpath, token_id
+		)
+		ORDER BY token_id
+	WHERE document_id = $documentid AND (user_id = 'bkrautgartner@oeaw.ac.at' OR user_id is NULL) 
+	GROUP BY 1, 2, 3 ) t1");
 $query->execute();
 //$result = $query->fetch(PDO::FETCH_COLUMN);
 $result = $query->fetch(PDO::FETCH_COLUMN);
