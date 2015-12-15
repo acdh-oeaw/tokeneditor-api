8c34bafc3477f6a15c394c9570d871b7bbb6cd5f
Mateusz Żółtak <zozlak@zozlak.org>
2015-12-15 15:24:32 +0100

    Server-side API endpoint for bulk updates of property values added (document/{docId}/{propName})

--- php/interface.php
+++ php/interface.php
@@ -111,54 +111,115 @@ if(filter_input(INPUT_SERVER, 'REQUEST_METHOD') === 'GET'){
 		)));
 	}
 }else if(filter_input(INPUT_SERVER, 'REQUEST_METHOD') === 'POST'){
-	//IMPORT
-	try{
-		if(!isset($_FILES['document']) || !isset($_FILES['schema'])){
-			throw new RuntimeException('document or schema not uploaded');
-		}
-		
-		$PDO->beginTransaction();
-		$doc = new import\Document($PDO);
-		$doc->loadFile(
-			$_FILES['document']['tmp_name'], 
-			$_FILES['schema']['tmp_name'],
-			filter_input(INPUT_POST, 'name')
-		);
-		$n = $doc->save($CONFIG['storageDir']);
-		
-		$query = $PDO->prepare("SELECT count(*) FROM users WHERE user_id = ?");
-		$query->execute(array(filter_input(INPUT_SERVER, $CONFIG['userid'])));
-		if($query->fetch(PDO::FETCH_COLUMN) == 0){
-			$query = $PDO->prepare("INSERT INTO users (user_id) VALUES (?)");
-			$query->execute(array(filter_input(INPUT_SERVER, $CONFIG['userid'])));
-		}
-		
-		$query = $PDO->prepare("INSERT INTO documents_users (document_id, user_id) VALUES (?, ?)");
-		$query->execute(array(
-			$doc->getId(), 
-			filter_input(INPUT_SERVER, $CONFIG['userid'])
-		));
-		header('Content-type: application/json');
-		if($n > 0){
-			$PDO->commit();
-			echo json_encode(array(
-				'status' => 'OK',
-				'document_id' => $doc->getId(),
-				'name' => filter_input(INPUT_POST, 'name'),
-				'tokensCount' => $n
-			));
-		}else{
-			$PDO->rollBack();
-			echo json_encode(array(
+	$docId = filter_input(INPUT_GET, 'document_id');
+	$propName = filter_input(INPUT_GET, 'property_name');
+	if($docId != '' && $propName != ''){
+		try{
+			// BULK UPDATE
+			$query = $PDO->prepare("SELECT property_xpath FROM properties WHERE document_id = ? AND name = ?");
+			$query->execute(array($docId, $propName));
+			$propXPath = $query->fetch(PDO::FETCH_COLUMN);
+			$tokenIds = explode(',', filter_input(INPUT_POST, 'tokenid'));
+			if($tokenIds[0] == '' || $propXPath == ''){
+				throw new RuntimeException('wrong property name or empty token id list');
+			}else{
+				$value  = filter_input(INPUT_POST, 'value');
+				$userId = filter_input(INPUT_SERVER, $CONFIG['userid']);
+				$tokenIdsStr = mb_substr(str_repeat(', ?', count($tokenIds)), 2);
+
+				$PDO->beginTransaction();
+
+				// update existing
+				$queryStr = "
+					UPDATE values 
+					SET value = ?, date = now()
+					WHERE 
+						document_id = ? 
+						AND property_xpath = ? 
+						AND user_id = ? 
+						AND value != ?
+						AND token_id IN (" . $tokenIdsStr . ")";
+				$query = $PDO->prepare($queryStr);
+				$query->execute(array_merge(array($value, $docId, $propXPath, $userId, $value), $tokenIds));
+
+				// add missing
+				$queryStr = "
+					INSERT INTO values (document_id, property_xpath, token_id, user_id, value, date)
+					SELECT document_id, property_xpath, token_id, ?::text, ?::text, now()
+					FROM
+						documents
+						JOIN properties USING (document_id)
+						JOIN tokens USING (document_id)
+						LEFT JOIN values USING (document_id, token_id, property_xpath)
+					WHERE
+						document_id = ?
+						AND property_xpath = ?
+						AND token_id IN (" . $tokenIdsStr . ")
+						AND user_id IS NULL";
+				$query = $PDO->prepare($queryStr);
+				$query->execute(array_merge(array($userId, $value, $docId, $propXPath), $tokenIds));
+
+				$PDO->commit();
+				echo json_encode(array(
+					'status' => 'OK'
+				));
+			}
+		}catch(Exception $ex){
+			exit(json_encode(array(
 				'status' => 'ERROR',
-				'message' => 'no tokens found - maybe your schema is wrong'
-			));
+				'message' => $ex->getMessage()
+			)));
 		}
-	} catch (Exception $ex) {
-		exit(json_encode(array(
-			'status' => 'ERROR',
-			'message' => $ex->getMessage()
-		)));
+	}else{
+		// IMPORT
+		try{
+			if(!isset($_FILES['document']) || !isset($_FILES['schema'])){
+				throw new RuntimeException('document or schema not uploaded');
+			}
+
+			$PDO->beginTransaction();
+			$doc = new import\Document($PDO);
+			$doc->loadFile(
+				$_FILES['document']['tmp_name'], 
+				$_FILES['schema']['tmp_name'],
+				filter_input(INPUT_POST, 'name')
+			);
+			$n = $doc->save($CONFIG['storageDir']);
+
+			$query = $PDO->prepare("SELECT count(*) FROM users WHERE user_id = ?");
+			$query->execute(array(filter_input(INPUT_SERVER, $CONFIG['userid'])));
+			if($query->fetch(PDO::FETCH_COLUMN) == 0){
+				$query = $PDO->prepare("INSERT INTO users (user_id) VALUES (?)");
+				$query->execute(array(filter_input(INPUT_SERVER, $CONFIG['userid'])));
+			}
+
+			$query = $PDO->prepare("INSERT INTO documents_users (document_id, user_id) VALUES (?, ?)");
+			$query->execute(array(
+				$doc->getId(), 
+				filter_input(INPUT_SERVER, $CONFIG['userid'])
+			));
+			header('Content-type: application/json');
+			if($n > 0){
+				$PDO->commit();
+				echo json_encode(array(
+					'status' => 'OK',
+					'document_id' => $doc->getId(),
+					'name' => filter_input(INPUT_POST, 'name'),
+					'tokensCount' => $n
+				));
+			}else{
+				$PDO->rollBack();
+				echo json_encode(array(
+					'status' => 'ERROR',
+					'message' => 'no tokens found - maybe your schema is wrong'
+				));
+			}
+		} catch (Exception $ex) {
+			exit(json_encode(array(
+				'status' => 'ERROR',
+				'message' => $ex->getMessage()
+			)));
+		}
 	}
 }else{
 	exit(json_encode(array(
